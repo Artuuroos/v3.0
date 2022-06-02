29684fa12602f43187ebdddf2836f321b56dba81
Artuuroos <arturos@web.de>
2022-06-02 08:25:10 +0200

    Zweite Website

---
+++ websitelaunch2.py
@@ -0,0 +1,48 @@
+from turtle import color
+import streamlit as st
+import plotly.figure_factory as ff
+import numpy as np
+import pandas as pd
+import plotly.express as px
+from streamlit_option_menu import option_menu
+
+optionen2 = option_menu(menu_title=None,
+                       options=["Home","Diagramm","Price","Notifi"],
+                       icons=["house","graph-up","clock","alarm"],
+                       menu_icon="cast",
+                       default_index=0,
+                       orientation="horizontal",
+                       )
+if optionen2=="Home":
+    st.write("Homepage")
+    
+else:
+    if optionen2=="Diagramm":
+        with st.sidebar:
+            optionenside= option_menu(menu_title=None,
+            options=["Liniendiagramm","Säulendiagramm"],
+            icons=["graph-up","bar-chart-line"],
+            menu_icon="",
+            default_index=0,
+            orientation="vertical",
+        )
+        if optionenside=="Liniendiagramm":
+            st.write("Hier kommt ein Liniendiagramm hin")
+            with st.form(key='form1'):
+                submit_button = st.form_submit_button(label='Graph ansehen')
+                if submit_button:
+                    st.write("Knopf gedrückt")
+                    
+        else:
+            
+            st.write("Hier kommt ein Säulendiagramm hin")
+            with st.form(key='form1'):
+                submit_button1 = st.form_submit_button(label='Graph ansehen')
+                if submit_button1:
+                    st.write("Säulendiagramm angezeigt")  
+                    
+    if optionen2=="Price":
+        st.write("Preisantizipation")
+        
+    if optionen2=="Notifi":
+        st.markdown("Benachrichtigung")          
