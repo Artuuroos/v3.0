700d139b2ab7daf029c8e7f7494094cdf8c037d3
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-02 23:06:07 +0200

    Update websitelaunch2.py

--- websitelaunch2.py
+++ websitelaunch2.py
@@ -175,6 +175,72 @@ if optionen2=="Home":
 
     zielbahn=st.selectbox("Zielbahnhof auswählen", optionliste)
     st.write("Ihr Zielbahhof ist:", zielbahn)
+    with st.form(key='form1'):
+    submit_button = st.form_submit_button(label='Submit')
+    
+    if submit_button:
+      start=option
+      ziel=zielbahn
+      datum=losdatum.strftime("%d.%m.%Y") 
+      uhrzeit_stunde=str(uhrzeit_stunde1)
+      uhrzeit_minuten=str(uhrzeit_minuten1)
+
+      uhrzeit_minuten=str(uhrzeit_minuten1)
+      if alter_1 in range(15,5,-1):
+            alter="f"
+      else: 
+        if alter_1 in range(26,13,-1): 
+                 alter="y"
+        else:
+                if alter_1 in range(64,26,-1):
+                    alter="e"
+                else: 
+                    alter="s" 
+
+
+      if bahnkarteneu=="50":
+        bahnkarte="4"
+       else: 
+        if bahnkarteneu=="25":
+                bahnkarte="2"
+        else: 
+                bahnkarte="0"
+      while True:
+
+        url='https://reiseauskunft.bahn.de/bin/query.exe/dn?revia=yes&existOptimizePrice-deactivated=1&country=DEU&dbkanal_007=L01_S01_D001_qf-bahn-svb-kl2_lz03&start=1&protocol=https%3A&REQ0JourneyStopsS0A=1&S='+start+'&REQ0JourneyStopsSID=A%3D1%40O%3DM%C3%BCnchen+Hbf%40X%3D11558339%40Y%3D48140229%40U%3D80%40L%3D008000261%40B%3D1%40p%3D1652295202%40&REQ0JourneyStopsZ0A=1&Z='+ziel+'&REQ0JourneyStopsZID=A%3D1%40O%3DAachen+Hbf%40X%3D6091495%40Y%3D50767803%40U%3D80%40L%3D008000001%40B%3D1%40p%3D1652295202%40&date=Fr%2C+'+datum+'&time='+uhrzeit_stunde+'%3A'+uhrzeit_minuten+'&timesel=depart&returnDate=&returnTime=&returnTimesel=depart&optimize=0&auskunft_travelers_number=1&tariffTravellerType.1='+alter+'&tariffTravellerReductionClass.1='+bahnkarte+'&tariffClass=2&rtMode=DB-HYBRID&externRequest=yes&HWAI=JS%21js%3Dyes%21ajax%3Dyes%21&externRequest=yes&HWAI=JS%21js%3Dyes%21ajax%3Dyes%21#hfsseq1|gl.0263982.1652621988'
+        source=requests.get(url)
+        soup = BeautifulSoup(source.text,"html.parser")
+
+        zugverbindungen=soup.find("div",class_="resultContentHolder")
+        zugverbindung1= zugverbindungen.find("div", class_="connectionData")
+        stationen_zugverbindung1=zugverbindung1.find("div", class_="connectionStations").get_text(strip=True)
+        uhrzeit_zv1= zugverbindung1.find("div", class_= "connectionTime")
+        abfahrt_zv1=uhrzeit_zv1.find("div", class_="time timeDep").get_text(strip=True)
+        ankunft_zv1=uhrzeit_zv1.find("div", class_="time timeArr").get_text(strip=True)
+        art_zug_zv1=soup.find("div", class_="products").get_text(strip=True)
+        preis_zv1=soup.find("div", class_="overviewConnection")
+        sparpreis_zv1=preis_zv1.find("div", class_="connectionPrice").get_text(strip=True)
+        sparpreis2_zv1=sparpreis_zv1.replace("ab","")
+
+        if "THA" in art_zug_zv1:
+
+            print("Diese Zugverbindung wird nicht von uns unterstüzt. Bitte wählen Sie eine Verbindung der Züge von der DB.")
+            break
+
+        else:
+            if "VRS-Tarif" in sparpreis_zv1:
+
+                print ("Hier ist kein Vergleich notwendig, da diese Verbindung zu VRS-Tarifen angeboten wird.")
+                break 
+
+            else: 
+
+                anfrage= time.strftime("%d.%m. %H:%M")
+                print(anfrage)
+                print("Stationen: ",stationen_zugverbindung1)
+                print("Fahrzeit: ",abfahrt_zv1,ankunft_zv1)
+                print("Art des Zuges/der Züge: ",art_zug_zv1)
+                print("Die Verbindung kostet: ",sparpreis_zv1)
 
 
 else:
